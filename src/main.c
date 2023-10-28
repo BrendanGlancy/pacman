2f1beefaceeeafb6eb768734336b34c93e6c02ee
brendanglancy <bglance68@gmail.com>
2023-10-28 17:05:53 -0400

    remove the sprites and use images

--- src/main.c
+++ src/main.c
@@ -12,12 +12,12 @@
 
 SDL_Window *window = NULL;
 SDL_Renderer *renderer = NULL;
+SDL_Texture *mazeTexture = NULL;
+
+int prevPacmanX = -1;
+int prevPacmanY = -1;
 
 void render_maze() {
-  SDL_SetRenderDrawColor(renderer, BLACK_COLOR.r, BLACK_COLOR.g, BLACK_COLOR.b,
-                         BLACK_COLOR.a);
-  SDL_RenderClear(renderer);
-
   SDL_SetRenderDrawColor(renderer, BLUE_COLOR.r, BLUE_COLOR.g, BLUE_COLOR.b,
                          BLUE_COLOR.a);
 
@@ -39,11 +39,22 @@ void render_maze() {
                            y + CELL_SIZE);
     }
   }
+}
+
+void render_pacman() {
+  if (prevPacmanX != -1 && prevPacmanY != -1) {
+    filledCircleRGBA(renderer, prevPacmanX, prevPacmanY, CELL_SIZE / 2.5,
+                     BLACK_COLOR.r, BLACK_COLOR.g, BLACK_COLOR.b,
+                     BLACK_COLOR.a);
+  }
 
   filledCircleRGBA(renderer, pacman.x * CELL_SIZE + CELL_SIZE / 2,
                    pacman.y * CELL_SIZE + CELL_SIZE / 2, CELL_SIZE / 2.5,
                    PACMAN_COLOR.r, PACMAN_COLOR.g, PACMAN_COLOR.b,
                    PACMAN_COLOR.a);
+
+  prevPacmanX = pacman.x * CELL_SIZE + CELL_SIZE / 2;
+  prevPacmanY = pacman.y * CELL_SIZE + CELL_SIZE / 2;
 }
 
 int main() {
@@ -66,7 +77,8 @@ int main() {
     return 1;
   }
 
-  renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+  renderer = SDL_CreateRenderer(
+      window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
   if (!renderer) {
     fprintf(stderr, "Error creating SDL renderer: %s\n", SDL_GetError());
     SDL_DestroyWindow(window);
@@ -74,9 +86,27 @@ int main() {
     return 1;
   }
 
+  // Initialize mazeTexture before using it as a render target
+  mazeTexture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888,
+                                  SDL_TEXTUREACCESS_TARGET, WIDTH * CELL_SIZE,
+                                  HEIGHT * CELL_SIZE);
+
+  // Render the maze to the texture
+  SDL_SetRenderTarget(renderer, mazeTexture);
+  SDL_SetRenderDrawColor(renderer, BLACK_COLOR.r, BLACK_COLOR.g, BLACK_COLOR.b,
+                         BLACK_COLOR.a);
+  SDL_RenderClear(renderer);
+  render_maze();
+  SDL_SetRenderTarget(renderer, NULL);
+
+  const int FPS = 60;
+  const int FRAME_DELAY = 5000 / FPS;
+
   int game_loop = 1;
   SDL_Event event;
   while (game_loop) {
+    Uint32 frameStart = SDL_GetTicks();
+
     while (SDL_PollEvent(&event)) {
       handle_input(event);
       if (event.type == SDL_QUIT)
@@ -85,11 +115,18 @@ int main() {
 
     key_input();
     update_pacman_position();
-    render_maze();
+
+    SDL_RenderCopy(renderer, mazeTexture, NULL, NULL);
+    render_pacman();
     SDL_RenderPresent(renderer);
-    SDL_Delay(FPS_DELAY);
+
+    Uint32 frameTime = SDL_GetTicks() - frameStart;
+    if (FRAME_DELAY > frameTime) {
+      SDL_Delay(FRAME_DELAY - frameTime);
+    }
   }
 
+  SDL_DestroyTexture(mazeTexture);
   SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
   SDL_Quit();
